98da921d63883855fda807fb3de5754dc533238f
olhazamlynska <olya.sonechko@gmail.com>
2022-09-08 19:08:39 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,18 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bodyRef = document.querySelector("body");
+console.log(bodyRef);
+
+const btnChangeRef = document.querySelector(".change-color");
+console.log(btnChangeRef);
+
+const coloNumberRef = document.querySelector(".color");
+console.log(coloNumberRef);
+
+const handleChangeColor = (event) => {
+  bodyRef.style.backgroundColor = getRandomHexColor();
+  coloNumberRef.textContent = getRandomHexColor();
+};
+
+btnChangeRef.addEventListener("click", handleChangeColor);
