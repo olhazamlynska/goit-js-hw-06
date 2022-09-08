e8d78a7946a3bae1b8e2bd18a50f849027bd12a2
olhazamlynska <olya.sonechko@gmail.com>
2022-09-08 17:09:16 +0300

    Fixed task5

--- js/task-05.js
+++ js/task-05.js
@@ -2,10 +2,10 @@ const inputRef = document.querySelector("#name-input");
 console.log(inputRef);
 const nameRef = document.querySelector("#name-output");
 console.log(nameRef);
-
-inputRef.addEventListener("input", (event) => {
+const handleText = (event) => {
   nameRef.textContent = event.currentTarget.value;
   if (event.currentTarget.value === "") {
     nameRef.textContent = "Anonymous";
   }
-});
+};
+inputRef.addEventListener("input", handleText);
