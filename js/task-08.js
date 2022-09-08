7fac7c82041d45a949a4a7ff28fe5010e4c92917
olhazamlynska <olya.sonechko@gmail.com>
2022-09-08 18:56:32 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formRef = document.querySelector(".login-form");
+console.log(formRef);
 
+formRef.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Попередження! Всі дані повинні бути заповнені");
+  }
+  const userObj = { email: email.value, password: password.value };
+  console.log(userObj);
+
+  event.currentTarget.reset();
+}
