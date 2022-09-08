b97fb0a760b1e728baa8588343afe59cb248bf1d
olhazamlynska <olya.sonechko@gmail.com>
2022-09-08 15:43:51 +0300

    Update task2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+const listRef = document.querySelector("#ingredients");
+
+const ingredientsItem = ingredients.map((name) => {
+  const item = document.createElement("li");
+  item.textContent = name;
+  item.classList.add("item");
+  console.log(item);
+  return item;
+});
+
+console.log(ingredientsItem);
+
+listRef.append(...ingredientsItem);
